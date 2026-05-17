525c3f3fbef1749718fa1f116615b5fce4d3fff0
timprox2-art <timprox2@gmail.com>
2026-05-17 15:01:52 +0300

    Delete Создать бота directory

--- Создать бота/config.py
+++
@@ -1,3 +0,0 @@
-BOT_TOKEN = "8366221636:AAGwIFmgISxqWrbNcXremtcgxXCu3stI8O0"
-ADMIN_ID = 5126359063  # Поменяй на свой реальный ТГ ID (через @userinfobot)
-MY_TELEGRAM_LINK = "https://t.me/timprox2"  # Твоя ссылка без @
